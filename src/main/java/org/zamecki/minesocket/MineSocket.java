109ce37115d12df51cf024f932ca08e3a6e67695
PedroZamecki <pedrozamecki@gmail.com>
2025-01-27 19:37:22 -0300

    add `LangController.java` to control languages

--- src/main/java/org/zamecki/minesocket/MineSocket.java
+++ src/main/java/org/zamecki/minesocket/MineSocket.java
@@ -2,12 +2,18 @@ package org.zamecki.minesocket;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
+import org.zamecki.minesocket.controller.LangController;
 
 public class MineSocket implements ModInitializer {
+    String MOD_ID = "minesocket";
     Logger logger = org.slf4j.LoggerFactory.getLogger("MineSocket");
-    
+    LangController langController;
+
     @Override
     public void onInitialize() {
         logger.info("MineSocket is initializing");
+
+        // Initialize the LangController
+        langController = new LangController(MOD_ID, logger);
     }
 }
---
+++ src/main/java/org/zamecki/minesocket/controller/LangController.java
@@ -0,0 +1,68 @@
+package org.zamecki.minesocket.controller;
+
+import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LangController {
+    Path configPath;
+
+    public LangController(String modId, Logger logger) {
+        logger.info("LangController is initializing");
+        configPath = Paths.get("config/" + modId + "/lang");
+        createConfigDirectory(logger);
+        copyAllLanguageFiles(modId, logger);
+    }
+
+    private void createConfigDirectory(Logger logger) {
+        try {
+            Files.createDirectories(configPath);
+            logger.info("Config directory created");
+        } catch (IOException e) {
+            logger.error("Error creating config directory", e);
+        }
+    }
+
+    private void copyAllLanguageFiles(String modId, Logger logger) {
+        getLanguageFilesToCopy(modId, logger).forEach(path -> copyLanguageFile(modId, logger, path));
+    }
+
+    private List<Path> getLanguageFilesToCopy(String modId, Logger logger) {
+        try (var stream = getClass().getResourceAsStream("/assets/" + modId + "/lang")) {
+            if (stream == null) {
+                logger.error("Resource path not found");
+                return List.of();
+            }
+            return new BufferedReader(new InputStreamReader(stream))
+                .lines()
+                .filter(line -> line.endsWith(".json"))
+                .filter(path -> !Files.exists(configPath.resolve(path)))
+                .map(Paths::get)
+                .toList();
+        } catch (IOException e) {
+            logger.error("Error getting language files", e);
+            return List.of();
+        }
+    }
+
+    private void copyLanguageFile(String modId, Logger logger, Path filePath) {
+        try (InputStream stream = getClass().getResourceAsStream("/assets/" + modId + "/lang/" + filePath.getFileName())) {
+            if (stream == null) {
+                logger.error("Resource path not found");
+                return;
+            }
+
+            Files.copy(stream, configPath.resolve(filePath));
+            logger.info("Language file copied: {}", filePath);
+        } catch (IOException e) {
+            logger.error("Error copying language file", e);
+        }
+    }
+}
