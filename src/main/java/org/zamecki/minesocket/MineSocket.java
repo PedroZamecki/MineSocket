76b09c9d3d4b63d5ac13b78ab1e128a40b8d5d71
PedroZamecki <pedrozamecki@gmail.com>
2025-01-27 19:57:26 -0300

    add event on player join

--- src/main/java/org/zamecki/minesocket/MineSocket.java
+++ src/main/java/org/zamecki/minesocket/MineSocket.java
@@ -1,6 +1,8 @@
 package org.zamecki.minesocket;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.zamecki.minesocket.controller.LangController;
 
@@ -15,5 +17,17 @@ public class MineSocket implements ModInitializer {
 
         // Initialize the LangController
         langController = new LangController(MOD_ID, logger);
+
+        // Register events callbacks
+        registerEventsCallbacks();
+    }
+
+    private void registerEventsCallbacks() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            var player = handler.getPlayer();
+            if (player.getPermissionLevel() >= server.getOpPermissionLevel()) {
+                player.sendMessage(Text.translatableWithFallback(MOD_ID + ".callback.on_op_join", "You are using MineSocket, you can configure/use the mod by using the '/ms' command "));
+            }
+        });
     }
 }
